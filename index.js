bcb1ec1cfe58644e9c826d0ce6edc51f44d8bdce
Udies <udimorde@gmail.com>
2025-07-29 18:27:17 +0000

    voice agent

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 app.get('/voice', (req, res) => {
   res.set('Content-Type', 'text/xml');
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
-  <Response>
-    <Start>
-      <Stream url="wss://your-server.com/ws" />
-    </Start>
-    <Say language="he-IL" voice="Google-Hebrew">שלום, מדבר סוכן הבינה המלאכותית. איך אפשר לעזור?</Say>
-  </Response>`);
+<Response>
+  <Start>
+    <Stream url="wss://your-server.com/ws" />
+  </Start>
+  <Say language="he-IL" voice="Google-Hebrew">שלום, מדבר סוכן הבינה המלאכותית. איך אפשר לעזור?</Say>
+</Response>`);
 });
 
-app.listen(3000, '0.0.0.0', () => console.log('Server ready at http://localhost:3000'));
+app.listen(3000, () => console.log('Server ready at http://localhost:3000'));
